2fe0bbf1f145a989046f7a37d5a4d27e1960e4e5
Md. Tanvir Ahmed <tanvirahmed.cse.bubt@gmail.com>
2019-11-13 18:41:34 +0600

    added MVCCore learning folder

---
+++ MVCCore/PracticeWithDatabase/PracticeWithDatabase/DatabaseOperation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWithDatabase
+{
+    class DatabaseOperation
+    {
+        private string conn = null;
+        public DatabaseOperation(string conn)
+        {
+            this.conn = conn;
+        }
+
+        public void ExecuteDDLQuery(string sql)
+        {
+            using(var connection = new SqlConnection(conn))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection?.Close();
+                }
+
+            }
+        }
+        public List<Dictionary<string, object>> GetData(string sql)
+        {
+            var dataTable = new List<Dictionary<string, object>>();
+            using (var connection = new SqlConnection(conn))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        using(var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var row = new Dictionary<string, object>();
+                                for (int i = 0; i< reader.FieldCount; i++)
+                                {
+                                    row[reader.GetName(i)] = reader[i];
+                                }
+                                dataTable.Add(row);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection?.Close();
+                }
+
+            }
+            return dataTable;
+        }
+    }
+}
---
+++ MVCCore/PracticeWithDatabase/PracticeWithDatabase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWithDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn = "Server = DESKTOP-1MF09JL; Database = Marketing; User Id = C#Aplication; Password = abc;";
+            DatabaseOperation databaseOperation = new DatabaseOperation(conn);
+            string insertQurey = "Insert into Subscription([Name]) values('Post notification');";
+            string selectQuery = "select * from Subscription;";
+            //databaseOperation.ExecuteDDLQuery(insertQurey);
+            var data = databaseOperation.GetData(selectQuery);
+            int flg = 0;
+            foreach (var row in data)
+            {
+                string rw = "";
+                string ky = "";
+                foreach(var item in row)
+                {
+                    rw += item.Value.ToString() + "    ";
+                    if(flg == 0) ky += item.Key.ToString() + "    ";
+                }
+                if (flg == 0)
+                {
+                    Console.WriteLine(ky);
+                    flg = 1;
+                }
+                Console.WriteLine(rw);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
